50aeb6beee33f303498c6c35d67a7cc11d364816
Cauã <cauadeassis2009@gmail.com>
2026-05-20 23:37:51 +0000

    Fixes `CORS` bug

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import errorHandling from "./middlewares/errorHandler";
 import routes from "./routes"
+import cors from "cors"
 
 const app = express();
+app.use(cors({
+    origin: "https://entregas-wine.vercel.app"
+}))
 app.use(express.json());
 app.use(routes)
 app.use(errorHandling)
